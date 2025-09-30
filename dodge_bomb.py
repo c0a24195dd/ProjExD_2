83e844d38429220c3a88a8d09d099778ac15652b
c0a24195dd <c0a24195dd@edu.teu.ac.jp>
2025-09-30 15:19:01 +0900

    移動できるようにした

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,14 +1,15 @@
 import os
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1100, 650
 DELTA ={
     pg.K_UP:(0,-5),
-    pg.k_DOWN:(0,+5),
+    pg.K_DOWN:(0,+5),
     pg.K_LEFT:(-5,0),
-    pg.k_RIGHT:(+5+0),
+    pg.K_RIGHT:(+5,0),
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,6 +21,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_img.set_colorkey((0,0,0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0,WIDTH)
+    bb_rct.centery = random.randint(0,HEIGHT)
+    vx,vy = +5,+5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -30,6 +38,10 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+        for key ,mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
         # if key_lst[pg.K_UP]:
         #     sum_mv[1] -= 5
         # if key_lst[pg.K_DOWN]:
